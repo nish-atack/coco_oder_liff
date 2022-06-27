28709de81dd7384077e0f7abec9da3bb2e385af6
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-27 15:30:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var key = params.get('key');
 
 $(function () {
     // 送信1
-    $('#form1').submit(function () {
+    $('#form1 :input').submit(function () {
         var reasons = $('textarea[name="reasons"]').val();
         var time0 = $('input[name="time1"]').val();
         var num1 = $('input[name="num1"]').val();
