0beffcaa50a645844b28dc8485d5cace8968f53b
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-27 16:43:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var params = (new URL(document.location)).searchParams;
 var key = params.get('key');
 
-        var reasons = $('textarea[name="reasons"]').val();
-        var time0 = $('input[name="time1"]').val();
-        var num1 = $('input[name="num1"]').val();
-        var breed = $('input[name="breed"]').val();
+
 
 
 $(function () {
     // 送信
     $('#form1').submit(function () {
-       // var reasons = $('textarea[name="reasons"]').val();
-       // var time0 = $('input[name="time1"]').val();
-      //  var num1 = $('input[name="num1"]').val();
-       // var breed = $('input[name="breed"]').val();
+        var reasons = $('textarea[name="reasons"]').val();
+        var time0 = $('input[name="time1"]').val();
+        var num1 = $('input[name="num1"]').val();
+        var breed = $('input[name="breed"]').val();
         
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
@@ -31,19 +28,19 @@ $(function () {
        
     });
     
-    
-    
-    
-    
-    
-    
-    
-     // 送信
+});
+
+
+
+
+
+$(function () {
+    // 送信
     $('#form2').submit(function () {
-        var reasons = $('textarea[name="reasons"]').val();
-        var time0 = $('input[name="time2"]').val();
-        var num1 = $('input[name="num2"]').val();
-        var breed = $('input[name="breed2"]').val();
+       // var reasons = $('textarea[name="reasons"]').val();
+       // var time0 = $('input[name="time2"]').val();
+        var num2 = $('input[name="num2"]').val();
+        var breed2 = $('input[name="breed2"]').val();
         
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
@@ -55,17 +52,15 @@ $(function () {
         
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-    var msg = `ご注文内容：\n${breed2} \n${num2} \n${time0} \n${reasons}`;
+    var msg = `ご注文内容：\n${breed2} \n${num2} \n${time2} \n${reasons2}`;
         sendText(msg);
-
         return false;
+       
     });
     
-    
-    
-    
-    
 });
+
+
 
 
 
