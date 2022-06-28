82a311786f93d8cb79d11f6698075f8fc7aa4103
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-28 10:54:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -50,21 +50,21 @@ $(function () {
               [breed5,num5,time5],
               [breed6,num6,time6]];
         
-        
-       sendText(kk)
-        
-  for(var i=0;i<6;i++){
-           if(kk[i][1] >= 1) {
-         msg = "種類："+ kk[i][0] + "\n数量：" + kk[i][1] + "\n受取時間：" + kk[i][2];  
-       
-    // sendText(msg); 
-               
       
-      //   return false;
-              }
+ msg =[]
+ for(var i=0;i<kk.length;i++){
+           if(kk[i][1] >= 1) {
+         msg[i] = ["種類："+ kk[i][0] + "\n数量：" + kk[i][1] + "\n受取時間：" + kk[i][2]];  
+           }else{
+    }
   }
         
-     
+        
+        
+        sendText(msg); 
+           
+        return false;
+        
     });
 });
 
